48caa0e599a09842318881cb0780b47903bfe5fe
khmalz <akmalrma1710@gmail.com>
2024-06-28 16:44:09 +0700

    update redirect if authenticated miiddleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RedirectIfAuthenticated::redirectUsing(function (Request $request) {
+            if ($request->user()->hasRole('admin')) {
+                return route('employee.index');
+            } elseif ($request->user()->hasRole('employee')) {
+                return route('benefit.index');
+            }
+
+            return '/';
+        });
     }
 }
