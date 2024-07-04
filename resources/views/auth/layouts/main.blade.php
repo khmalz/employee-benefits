c9cc5c2b5ca70df8f8f7e1a97e9d3fb7a01924ed
khmalz <akmalrma1710@gmail.com>
2024-07-04 10:21:57 +0700

    add toastify when login error

--- resources/views/auth/layouts/main.blade.php
+++ resources/views/auth/layouts/main.blade.php
@@ -20,6 +20,8 @@
             @yield('content')
         </div>
     </div>
+
+    @stack('scripts')
 </body>
 
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,31 @@
 @extends('auth.layouts.main')
 
 @section('content')
+    @error('email')
+        <div class="initial-toastify fixed right-1 top-1 flex w-full max-w-xs transform items-center rounded-lg bg-white p-4 text-gray-500 shadow transition-transform duration-300 ease-in-out md:right-5 md:top-5"
+            id="toast-top-right" role="alert">
+            <div class="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500">
+                <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                </svg>
+                <span class="sr-only">Error icon</span>
+            </div>
+            <div class="ms-3 text-sm font-normal">Email atau password salah</div>
+            <button
+                class="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300"
+                data-dismiss-target="#toast-top-right" type="button" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+            </button>
+        </div>
+    @enderror
+
+    <div id="fail" data-fail="{{ $errors->first('email') }}"></div>
+
     <h1 class="mb-5 text-3xl font-semibold">Login</h1>
     <form method="POST" action="{{ route('login') }}">
         @csrf
@@ -21,3 +46,22 @@
             type="submit">Login</button>
     </form>
 @endsection
+
+@push('scripts')
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            if (document.getElementById('fail').getAttribute('data-fail')) {
+                const toast = document.getElementById('toast-top-right');
+                setTimeout(() => {
+                    toast.classList.remove('initial-toastify');
+                    toast.classList.add('translate-y-0');
+                }, 200);
+
+                setTimeout(() => {
+                    toast.classList.add('initial-toastify');
+                    toast.classList.remove('translate-y-0');
+                }, 2200);
+            }
+        });
+    </script>
+@endpush
